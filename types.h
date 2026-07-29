2c1ef3cd9b0948490146ec826e2cc803f9c919ad
thaweru <thaweru@github.com>
2026-07-29 23:34:25 +0530

    added board_data.csv can be used to load board information from file

--- types.h
+++ types.h
@@ -1,78 +1,37 @@
 #ifndef TYPES_H
 #define TYPES_H
 
-struct Player;
-struct Property;
-struct InsurancePolicy;
-struct Loan;
-struct PlayerStratergy;
-
 //Player attributes
-struct Player{
+typedef struct Player{
+	char name[];
+	char strategy;
 	int cash;
 	int position;
 	char InJail;
 	int jailTurns;
-//	struct Property property[];
-//	struct Loan loan;
-//	struct InsurancePolicy policies[];
-//	struct PlayerStratergy stratergy;
-	char name[];
-};
+//	struct Loan;
+} plyr;
 
-//Property attributes
-struct Property{
-	int purchasePrice;
-	int morgageValue;
-	int baseRental;
+typedef union SquareType{
+	struct Property prpty;
+	struct 
+} sqrtype;
+
+typedef struct Square{
+	char type;
+	char name[];
+	char group;
+	int buyPrice;
+	int baseRent;
 	int houseCost;
 	int hotelCost;
-//	enum PropertyGroup group;
-	struct Player *owner;
-	char morgaged;
-	int numHouses;
-	char hasHotel;
-	int condition;
-	int age;
-};
-//Main structure of the program
-struct Game{
-	struct Player player[4];
-//	struct Board board;
-	int currentRound ;//= 0;
-//	struct EconomicState economy;
-};
+	plyr *owner;
+	char houses;
+} sqr;
 
-void startGame();
-void runRound();
-char isGameOver();
-struct Player determineWinner();
+typedef struct Board{
+	sqr s[40]
+} bd;
 
-
-void takeTurn();
-void purchaseProperty(struct Property);
-void payRent(int);
-void declareBankrupt();
-int calculateNetWorth();
-
-//Board attributes
-struct Board{
-//	struct Square square[40];
-};
-
-void initializeBoard();
-struct Square getSquare(int);
-
-struct Square{
-	int index;
-	char name[];
-//	union SquareType type;
-};
-
-
-int calculateRent(int);
-void construct();
-void morgage();
-void renovate();
 int dice(char *duble);
 #endif
