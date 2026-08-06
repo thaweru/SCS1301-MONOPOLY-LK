82de67c92905b627e7d4fd7c3d33b7947dd7ad01
thaweru <thaweru@github.com>
2026-08-07 00:01:56 +0530

    passed go logic need debugging

--- game.c
+++ game.c
@@ -12,8 +12,15 @@ void run_game(plyr *P, int N, int n, bd bord){
 			prev = P[i].pos;
 			P[i].pos += rolled;
 			printf("%s moves from Square %i to Square %i\n\n",
-				P[i].name, prev, P[i].pos
+				P[i].name, prev, P[i].pos%40
 				);
+			if (P[i].pos < 40){
+				P[i].cash += 2000;
+				printf("%s passed GO.\nCollected LKR 2,000.\nCurrent Balance : LKR %d.\n",
+					P[i].name, P[i].cash
+				);
+			P[i].pos = P[i].pos % 40;
+			}
 		}
 		currRound++;
 	}
