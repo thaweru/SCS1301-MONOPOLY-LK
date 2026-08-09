7b81f6805fa4c1324d9fd7266db3e5c430bce6e5
thaweru <dulmeth94@gmail.com>
2026-08-09 14:17:12 +0530

    refactor: Added game struct.
    It holds the state and data of all squares, players and game state.

--- types.h
+++ types.h
@@ -3,6 +3,7 @@
 
 #define PLAYERS 4
 #define MAX_ROUNDS 500
+#define SQUARES 40
 
 //Player attributes
 typedef struct Player{
@@ -52,6 +53,14 @@ typedef enum propertyGroup{
 	CD_FUND
 }sqrgrp;
 
+typedef enum EconomicState{
+    STABLE,
+    BOOM,
+    MID_I,
+    HIGH_I,
+    RECESSION,
+} econ;
+
 typedef struct Square{
 	sqrTyp type;
 	char name[42];
@@ -67,6 +76,13 @@ typedef struct Square{
 typedef struct Board{
 	sqr s[40];
 } bd;
+
+typedef struct Game{
+    sqr square[SQUARES];
+    plyr player[PLAYERS];
+    int currRound;
+    econ econState;
+} game;
 
 int dice(char *duble);
 bd init_game(int n, char **arr);
